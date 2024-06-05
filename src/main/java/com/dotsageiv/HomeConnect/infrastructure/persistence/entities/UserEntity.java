1d4d3d04b5f6e0d8d1569806c35e0a8736ef3963
dotsageiv <brendoxcentral@gmail.com>
2024-06-05 03:29:57 -0300

    user validation

--- src/main/java/com/dotsageiv/HomeConnect/infrastructure/persistence/entities/UserEntity.java
+++ src/main/java/com/dotsageiv/HomeConnect/infrastructure/persistence/entities/UserEntity.java
@@ -1,6 +1,8 @@
 package com.dotsageiv.HomeConnect.infrastructure.persistence.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.io.Serializable;
 import java.util.Objects;
@@ -13,10 +15,24 @@ public class UserEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Size(min = 14, max = 14, message = "O CPF deve conter exatamente 14 números.")
+    @Column(unique = true, nullable = false, length = 14)
+    @NotBlank(message = "O CPF é obrigatório.")
     private String cpf;
+
+    @Size(max = 75, message = "O nome de completo possui 75 caracteres ao máximo.")
+    @Column(nullable = false, length = 75)
+    @NotBlank(message = "O nome completo é obrigatório.")
     private String fullName;
 
+    @Size(max = 75, message = "O nome de usuário possui 75 caracteres ao máximo")
+    @Column(unique = true, nullable = false, length = 75)
+    @NotBlank(message = "O nome de usuário é obrigatório.")
     private String username;
+
+    @NotBlank(message = "A senha é obrigatório.")
+    @Column(nullable = false, length = 10)
+    @Size(max = 10, message = "A senha deve conter pelo menos 10 caracteres")
     private String password;
 
     public UserEntity() {}
